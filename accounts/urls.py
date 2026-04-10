9e334b6700847ce558287cc68d15a7e4ae2f8228
rupeshdahal07 <rupyesdahal12@gmail.com>
2026-04-10 07:55:39 +0545

    ✨ feat: add registration and logout views, update URL patterns

--- accounts/urls.py
+++ accounts/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path("register/", views.register, name="login" ),
     path("login/", views.login, name="login" ),
+    path("logout/", views.login, name="login" ),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
 
 # Create your views here.
+def register(request):
+    return render(request, "auth/register.html", status=200)
+
+
 def login(request):
     return render(request, "auth/login.html", status=200)
+
+def logout(request):
+    return render(request, "auth/logout.html", status=200)
