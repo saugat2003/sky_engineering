4cf13dd5bcc78bff9c1b2ae6703b546024dca2c4
rupeshdahal07 <rupyesdahal12@gmail.com>
2026-05-03 19:53:21 +0545

    ✨ feat: implement department overview page with updated routing and template structure ♻️ refactor: update navigation links to point to the new department overview page 🗑️ delete: remove obsolete organization overview template

--- department/views.py
+++ department/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 
 # Create your views here.
 def department_overview(request):
-    return render(request, "organization/overview.html")
+    return render(request, "department/overview.html")
